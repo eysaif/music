6b67faa846dbd811a9741efaaf416e0e4c94dfc7
eysaif <u4saif@gmail.com>
2023-09-17 16:25:36 +0530

    dark mode added change on toggle

--- src/app/components/nav/Nabar.tsx
+++ src/app/components/nav/Nabar.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Nabar = () => {
+type props = {
+  changeTheme: ()=>void;
+}
+const Nabar = ({changeTheme}:props) => {
   return (
     <nav>
       <div className="navbar bg-neutral text-neutral-content flex justify-between">
         <button className="btn btn-outline btn-accent">+ List</button>
         <span>
-          <input type="checkbox" className="toggle" />
+          <input type="checkbox" className="toggle" onClick={changeTheme} />
         </span>
       </div>
     </nav>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,38 @@
-'use client';
+"use client";
 
-import React,{ useState }  from "react";
+import React, { useState } from "react";
 import Nabar from "../components/nav/Nabar";
 import Footer from "../components/footer/Footer";
 import Card from "../components/card/Card";
 import Dialog from "../components/dialog/Dialog";
- 
+
 const Dashboard = () => {
-  const [videoUrl , setVideoUrl] = useState('');
-  const childEmiter = (childData :any )=>{
+  const [videoUrl, setVideoUrl] = useState("");
+  const [themeValue , setthemeValue] = useState('cupcake');
+  const childEmiter = (childData: any) => {
     // console.log("Emit from child",childData);
-    let correctURL = (childData.includes('?v='))  ? childData.split('?v=')[1].split("").splice(0,11).join("") : childData.split('.be/')[1].split("").splice(0,11).join("");
+    let correctURL = childData.includes("?v=")
+      ? childData.split("?v=")[1].split("").splice(0, 11).join("")
+      : childData.split(".be/")[1].split("").splice(0, 11).join("");
     setVideoUrl("https://www.youtube.com/embed/" + correctURL);
-  }
+  };
+
+  const UpdateTheme = () => {
+    themeValue == "halloween" ? setthemeValue("cupcake") : setthemeValue("halloween");
+    console.log(themeValue);
+  };
   return (
     <>
-      <div data-theme="cupcake">
-        <Nabar />
+      <div data-theme={themeValue}>
+        <Nabar changeTheme={UpdateTheme}/>
         <main className="flex m-2  flex-col gap-2 h-screen">
-          <Dialog childEmiter={childEmiter}/>
+          <Dialog childEmiter={childEmiter} />
           <select className="select select-bordered w-full max-w-xs">
             <option disabled>Select Your List</option>
             <option key="a">Normal Apple</option>
             <option key="o">Normal Orange</option>
           </select>
-          <Card vedioUrl={videoUrl}/>
+          <Card vedioUrl={videoUrl} />
         </main>
         {/* <Footer /> */}
       </div>
